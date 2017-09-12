be728d40c61dfc8dd3f410cdb2fa8801ab7b45c2
darrylweimers <30273976+darrylweimers@users.noreply.github.com>
2017-09-12 10:08:08 -0400

    BitTwiddler.h
    Bit operations

---
+++ Utilities/BitTwiddler/BitTwiddler.h
@@ -0,0 +1,19 @@
+//*****************************************
+// BitTwiddler.h
+//*****************************************
+
+#ifndef _BITTWIDDLER_H_
+#define _BITTWIDDLER_H_
+
+/* *************************************************************************
+ * DEFINES
+ * *************************************************************************
+ */
+
+#define BIT_TWIDDLER_BIT_VALUE(BIT_POS)			    (1 << BIT_POS)
+#define BIT_TWIDDLER_SET_BIT(BYTE, BIT_POS)   	(BYTE | BIT_TWIDDLER_BIT_VALUE(BIT_POS))
+#define BIT_TWIDDLER_CLEAR_BIT(BYTE, BIT_POS) 	(BYTE & (~BIT_TWIDDLER_BIT_VALUE(BIT_POS)))
+#define BIT_TWIDDLER_TOGGLE_BIT(BYTE, BIT_POS)  (BYTE ^ BIT_TWIDDLER_BIT_VALUE(BIT_POS))
+#define BIT_TWIDDLER_GET_BIT(BYTE, BIT_POS)   	((BYTE & BIT_TWIDDLER_BIT_VALUE(BIT_POS)) >> BIT_POS)
+
+#endif /* _BITTWIDDLER_H_ */
