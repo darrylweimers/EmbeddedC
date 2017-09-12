433e981e45c87e94f671ff2e7dbcf8f659294f18
darrylweimers <30273976+darrylweimers@users.noreply.github.com>
2017-09-12 12:06:59 -0400

    SoftwareTimer.h
    Create multiple software periodic/timeout functions through your micro controller's timer program to interrupt at every 1 ms/1us. 
    Define the number of software timer module @TODO:  // TODO: Define the maximum number of software timer

---
+++ Utilities/SoftwareTimer/SoftwareTimer.h
@@ -0,0 +1,53 @@
+//***********************************************
+//	SoftwareTimer.h
+//***********************************************
+
+#ifndef _SOFTWARETIMER_H_
+#define _SOFTWARETIMER_H_
+
+#include "GlobalDef.h"
+
+// REMARK: Timer Driver can only increment by step of 1 (e.g. every 1 ms, us, s , ...)
+
+/* ********************************************************************
+ * DEFINES
+ * *********************************************************************/
+
+#define MAX_NUMBER_OF_MODULES  8        // TODO: Define the maximum number of software timer 
+
+/* ********************************************************************
+ * TYPE
+ * *********************************************************************/
+
+// each module can be set to mode: periodic, timeout
+typedef enum
+{
+	SoftwareTimer_Type_Null,
+	SoftwareTimer_Type_Periodic,
+	SoftwareTimer_Type_Timeout
+}SoftwareTimer_Type;
+
+typedef struct
+{
+	volatile bool enabled[MAX_NUMBER_OF_MODULES];
+	volatile U32 count[MAX_NUMBER_OF_MODULES];
+	U32 max[MAX_NUMBER_OF_MODULES];
+	SoftwareTimer_Type type[MAX_NUMBER_OF_MODULES];
+	U8 totalModuleEnabled;
+	bool driverEnabled;
+	Func_Ptr_Enable driverEnable;
+	U8 numberOfModule;
+}SoftwareTimer;
+
+/* ********************************************************************
+ * PUBLIC FUNCTIONS DECLARATIONS
+ * *********************************************************************/
+
+bool SoftwareTimer_IsTriggered(SoftwareTimer* softwareTimer, U8 Module);
+void SoftwareTimer_Start(SoftwareTimer* softwareTimer, U8 Module);
+void SoftwareTimer_Stop(SoftwareTimer* softwareTimer, U8 Module);
+void SoftwareTimer_Init(SoftwareTimer* softwareTimer, U8 Module, U32 Max, SoftwareTimer_Type Type);
+void SoftwareTimer_InterruptHanlder(SoftwareTimer* softwareTimer);
+void SoftwareTimer_DriverInit(SoftwareTimer* softwareTimer,U8 TotalNumberOfModule, Func_Ptr_Void DriverInit,  Func_Ptr_Enable DriverEnable);
+
+#endif /* _SOFTWARETIMER_H_ */
