72e156c343c99d27bc33914070090c93c42c7ebf
darrylweimers <30273976+darrylweimers@users.noreply.github.com>
2017-09-12 10:44:54 -0400

    FrameBuffer.h
    Queue frame into a First In First Out array

---
+++ Utilities/Buffer/FrameBuffer/FrameBuffer.h
@@ -0,0 +1,41 @@
+//***********************************************
+// 	FrameBuffer.h
+//***********************************************
+
+#ifndef _FRAMEBUFFER_H_
+#define _FRAMEBUFFER_H_
+
+#include "compiler.h"
+
+/* ********************************************************************
+ * DEFINES
+ * *********************************************************************/
+
+#define FRAME_BUFFER_FIFO_FULL	FAIL
+#define FRAME_BUFFER_FIFO_EMPTY	FAIL
+#define FRAME_BUFFER_FIFO_OK	PASS
+
+#define FIFO_BUFFER_SIZE 2048		// MARK: determine value
+
+/* ********************************************************************
+ * TYPES
+ * *********************************************************************/
+
+typedef volatile struct
+{
+	volatile U32 TotalElement;				// Total of elements in fifo array
+	volatile U16 FrameCount;				// Total number of frame in fifo array
+	volatile U32 ReadPtr;					// Read location
+	volatile U32 WritePtr;					// Write location
+	volatile U8  Fifo[FIFO_BUFFER_SIZE];	// Queue
+}FIFO_FrameBuffer;
+
+/* ********************************************************************
+ * PUBLIC FUNCTION DECLARATIONS
+ * *********************************************************************/
+
+void FrameBuffer_Init(volatile FIFO_FrameBuffer * FrameFifo);
+U8 FrameBuffer_Push(volatile FIFO_FrameBuffer * FrameFifo, U8 Frame[], U32 SizeOfFrame);
+U8 FrameBuffer_Pop(volatile FIFO_FrameBuffer * FrameFifo, volatile U8 Frame[], volatile U32* SizeOfFrame);
+
+#endif /* FRAMEBUFFER_H_ */
