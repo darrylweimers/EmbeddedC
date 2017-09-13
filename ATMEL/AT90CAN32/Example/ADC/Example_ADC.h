8e54cb8a165cf3833558560858db113bf2147126
darrylweimers <30273976+darrylweimers@users.noreply.github.com>
2017-09-13 15:26:53 -0400

    Example_ADC.h

--- ATMEL/AT90CAN32/Example/ADC/Example_ADC.h
+++ ATMEL/AT90CAN32/Example/ADC/Example_ADC.h
@@ -1,1 +1,18 @@
 
+//***********************************************
+//	Example_ADC.h
+//***********************************************
+
+#ifndef _EXAMPLE_ADC_H_
+#define _EXAMPLE_ADC_H_
+
+#include "GlobalDef.h"
+#include "ADC.h"
+
+
+/* *************************************************************************
+ * PUBLIC FUNCTION DECLARATIONS
+ * *************************************************************************
+ */
+ U16 ADC_Read(ADC_Channel Channel);
+ void ADC_Init(void);
